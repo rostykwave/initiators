92fb7d872d0ae925b06a5973ff169c82b1af95a1
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-10-09 17:33:52 +0300

    POST

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Account } from './account.entity';
 import { AccountsService } from './accounts.service';
+import { CreateAccountDto } from './dto/create-account.dto';
 
 @Controller('accounts')
 export class AccountsController {
   constructor(private accountsService: AccountsService) {}
+
+  @Post()
+  create(@Body() createAccountDto: CreateAccountDto): Promise<Account> {
+    return this.accountsService.create(createAccountDto);
+  }
 
   @Get()
   findAll() {
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -2,13 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Account } from './account.entity';
+import { CreateAccountDto } from './dto/create-account.dto';
 
 @Injectable()
 export class AccountsService {
   constructor(
     @InjectRepository(Account)
-    private accountRepository: Repository<Account>,
+    private readonly accountRepository: Repository<Account>,
   ) {}
+
+  create(createAccountDto: CreateAccountDto): Promise<Account> {
+    const account = new Account();
+    account.firstName = createAccountDto.firstName;
+    account.lastName = createAccountDto.lastName;
+    account.email = createAccountDto.email;
+    account.password = createAccountDto.password;
+
+    return this.accountRepository.save(account);
+  }
 
   findAll(): Promise<Account[]> {
     return this.accountRepository.find();
