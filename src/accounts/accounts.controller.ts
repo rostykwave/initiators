f13085e281e67a827ac8665cb07df56e34c93e85
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-10-12 09:09:37 +0300

    getbyid, delete

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { Account } from './account.entity';
 import { AccountsService } from './accounts.service';
 import { CreateAccountDto } from './dto/create-account.dto';
@@ -15,5 +23,24 @@ export class AccountsController {
   @Get()
   findAll() {
     return this.accountsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.accountsService.findOne(id);
+  }
+
+  // @Put(':id')
+  // update(
+  //   @Param('id', ParseIntPipe)
+  //   id: number,
+  //   @Body() createAccountDto: CreateAccountDto,
+  // ) {
+  //   return this.accountsService.updateOne(id, updateBDto);
+  // }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.accountsService.remove(id);
   }
 }
