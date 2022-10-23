2917e2119fbd2eabfb6a6aa2edca59a75baa13e3
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-10-23 17:18:01 +0300

    process.env instead ConfigService

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AccountsService } from 'src/accounts/accounts.service';
-
-const configService = new ConfigService();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('SECRET'),
+      secretOrKey: process.env.SECRET,
     }); // config
   }
 
