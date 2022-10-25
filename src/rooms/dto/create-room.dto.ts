570e7a015e84ee4050e21cc8dedbb7a25630ee69
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-10-25 20:29:35 +0300

    rooms dto validation

--- src/rooms/dto/create-room.dto.ts
+++ src/rooms/dto/create-room.dto.ts
@@ -1,9 +1,23 @@
+import { IsString, IsNumber } from 'class-validator';
+
 export class CreateRoomDto {
   id?: number;
+
+  @IsString()
   name: string;
+
+  @IsNumber()
   floor: number;
+
+  @IsString()
   description: string;
+
+  @IsNumber()
   maxPeople: number;
+
+  @IsNumber()
   minPeople: number;
+
+  @IsString()
   officeName: string;
 }
