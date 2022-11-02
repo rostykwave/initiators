2028903939b98226c5c26bd6882b4c8c1d04693e
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-11-02 21:31:46 +0200

    DaysOfWeek enum update

--- src/recurringBookings/recurringBooking.entity.ts
+++ src/recurringBookings/recurringBooking.entity.ts
@@ -11,13 +11,13 @@ import {
 } from 'typeorm';
 
 export enum DaysOfWeek {
-  Monday = 'Monday',
-  Tuesday = 'Tuesday',
-  Wednesday = 'Wednesday',
-  Thursday = 'Thursday',
-  Friday = 'Friday',
-  Saturday = 'Saturday',
-  Sunday = 'Sunday',
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+  Sunday = 7,
 }
 
 @Entity()
