93eab8275fd819e3fd737143b87e2d7754a179fb
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-11-13 15:31:59 +0200

    INVITE_USER_REGISTRATION_LINK in .env

--- src/sendgridEmail/constants/sendgridEmail.constants.ts
+++ src/sendgridEmail/constants/sendgridEmail.constants.ts
@@ -1,5 +1,6 @@
-const INVITE_USER_REGISTRATION_LINK =
-  'https://initiators-ua.herokuapp.com/accounts';
-
-export const INVITE_USER_EMAIL_HTML = (email: string, password: string) =>
-  `<div><h1>We are pleased to invite you to our team.</h1><p>Continue registration with yours email: <b>${email}</b> and password: <b>${password}</b>.</p>Follow <a href=${INVITE_USER_REGISTRATION_LINK}>link to registration</a></div>`;
+export const INVITE_USER_EMAIL_HTML = (
+  email: string,
+  password: string,
+  registrationLink: string,
+) =>
+  `<div><h1>We are pleased to invite you to our team.</h1><p>Continue registration with yours email: <b>${email}</b> and password: <b>${password}</b>.</p>Follow <a href=${registrationLink}>link to registration</a></div>`;
